b2bb246eca9a650af89d971d8a600d2e4ad50de7
Hung Pham <hungpham28197@gmail.com>
2021-09-26 16:44:08 +0700

    redis cluster thu nghiem voi golang

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -23,6 +25,10 @@ func ExampleClient(w http.ResponseWriter, r *http.Request) {
 		},
 		Password:         "123",
 		SentinelPassword: "123",
+		RouteRandomly:    true,
+		ReadTimeout:      10 * time.Second,
+		WriteTimeout:     10 * time.Second,
+		PoolTimeout:      10 * time.Second,
 	})
 	defer rdb.Close()
 
@@ -38,24 +44,51 @@ func ExampleClient(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	_, _ = fmt.Fprintf(w, val)
-	// fmt.Println("key1", val)
+}
 
-	// val2, err := rdb.Get(ctx, "key2").Result()
-	// if err == redis.Nil {
-	// 	fmt.Println("key2 does not exist")
-	// } else if err != nil {
-	// 	panic(err)
-	// } else {
-	// 	fmt.Println("key2", val2)
-	// }
-	// Output: key value
-	// key2 does not exist}
+func ExampleRedisCluster(w http.ResponseWriter, r *http.Request) {
+	rdb := redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs: []string{
+			os.Getenv("redis-node-0") + ":6379",
+			os.Getenv("redis-node-1") + ":6379",
+			os.Getenv("redis-node-2") + ":6379",
+			os.Getenv("redis-node-3") + ":6379",
+			os.Getenv("redis-node-4") + ":6379",
+			os.Getenv("redis-node-5") + ":6379",
+		},
+		Password:      "bitnami",
+		RouteRandomly: true,
+		ReadTimeout:   10 * time.Second,
+		WriteTimeout:  10 * time.Second,
+		PoolTimeout:   10 * time.Second,
+	})
+	defer rdb.Close()
+
+	rdb.Ping(ctx)
+
+	for i := 0; i < 100; i++ {
+		err := rdb.Set(ctx, "redis-cluster-"+strconv.Itoa(i), "redis-cluster-test", 0).Err()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	for i := 0; i < 100; i++ {
+		val, err := rdb.Get(ctx, "redis-cluster-"+strconv.Itoa(i)).Result()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Key: " + "redis-cluster-" + strconv.Itoa(i) + "   Value: " + val)
+	}
+
+	_, _ = fmt.Fprintf(w, "Done")
 }
 
 func main() {
 
-	http.HandleFunc("/", ExampleClient)
+	http.HandleFunc("/", ExampleRedisCluster)
 	//db.Ping(ctx)
+	//http.HandleFunc("/", ExampleDockerSecret)
 	fmt.Println("Listen at 8001")
 	log.Fatal(http.ListenAndServe(":8001", nil))
 	fmt.Println("Done")
